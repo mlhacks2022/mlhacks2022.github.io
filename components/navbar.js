95f1a180744c3364947e4278a18c236ef981744f
Lilian Zhao <milanzq309@gmail.com>
2022-04-11 22:08:32 -0700

    started intro section

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+ 
+import React from 'react';
+import { useEffect, useRef, useState } from 'react';
+import '../styles/globals.css';
+
+export default function Navbar() {
+    return (
+        <div></div>
+    );
+};
