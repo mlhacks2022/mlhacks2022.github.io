88bf66d19a015772318fb8b639d9afb3a971e1f8
Lilian Zhao <milanzq309@gmail.com>
2022-04-14 19:24:07 -0700

    testbuild

--- pages/PageLoading.js
+++ pages/PageLoading.js
@@ -1,16 +1,8 @@
 import React from 'react'
-import Loader from 'react-loader-spinner'
 
 export default function PageLoading() {
     return (
         <div className="page-container">
-            <Loader
-                type="BallTriangle"
-                color="rgb(24, 40, 255)"
-                height={200}
-                width={100}
-                timeout={999999}
-            />
         </div>
     )
 }
