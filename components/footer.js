5f4b1345bfc5c87a740e79fe9021b27bb256dffa
Lilian Zhao <milanzq309@gmail.com>
2022-04-19 01:12:15 -0700

    added footer

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,54 @@
 import styles from '../styles/Footer.module.css';
 
+import Image from 'next/image';
+
+import { FaEnvelope, FaRegEnvelope, FaDiscord,
+    FaInstagram, FaInstagramSquare } from 'react-icons/fa';
+
 export default function Footer() {
     return (
-        <footer className={styles.footer}>
-            <p>Copyright © 2022 ML Hacks. All rights reserved.</p>
-            <a 
-                href="https://freeicons.io/profile/5790" 
-                rel="noopener noreferrer"
-                title="icons">Icons created by ColourCreatype - FreeIcons</a>
-            <a 
-                target="_blank" 
-                rel="noopener noreferrer"
-                href="mailto:mlhacks.22@gmail.com"> mlhacks.22@gmail.com</a>
-                <div id='contact' className={styles.contact + " section"}>
+        <footer id='contact' className={styles.footer}>
+            <div>
+                <div className={styles.contact}>
+                    <div className={styles.colContainer}>
+                        <h1 className={styles.title}>ML Hacks</h1>
+                    </div>
+                    <div className={styles.colContainer}>
+                        <div className={styles.col}>
+                            <a className={styles.link}
+                                href="https://freeicons.io/profile/5790" 
+                                rel="noopener noreferrer"
+                                title="icons">Home</a>
+                            <a className={styles.link}
+                                href="https://freeicons.io/profile/5790" 
+                                rel="noopener noreferrer"
+                                title="icons">Sponsor us</a>
+                        </div>
+                        <div className={styles.col}>
+                            <a className={styles.icon}
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                href="mailto:mlhacks.22@gmail.com"><FaEnvelope /></a>
+                            <a className={styles.icon}
+                                href="https://discord.gg/epVVJeUs" 
+                                target="_blank"
+                                rel="noopener noreferrer"><FaInstagram /></a>
+                            <a className={styles.icon}
+                                href="https://discord.gg/epVVJeUs" 
+                                target="_blank"
+                                rel="noopener noreferrer"><FaDiscord /></a>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div className={styles.disclaimer}>
+                <a 
+                    className={styles.link}
+                    href="https://freeicons.io/profile/5790" 
+                    rel="noopener noreferrer"
+                    title="icons">Icons created by ColourCreatype - FreeIcons</a>
+                <p>Copyright © 2022 ML Hacks | Made with the souls of a thousand diabetic bats</p>
+            </div>
         </footer>
     );
 };
--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,15 @@
 import Navbar from './navbar';
 import Footer from './footer';
+import { Parallax } from 'react-scroll-parallax';
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
         <main>{children}</main>
-      <Footer />
+      <Parallax>
+        <Footer />
+      </Parallax>
     </>
   )
 };
